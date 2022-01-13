bc2fd2eb91ec707c519efbb13f52baa5a6323485
LinusNTI <92534409+LinusNTI@users.noreply.github.com>
2022-01-13 15:02:07 +0100

    Added comments

--- Lottery/MyProgram.cs
+++ Lottery/MyProgram.cs
@@ -13,13 +13,15 @@ namespace Lottery
 
         public void Run() {
             bool running = true;
-            while (running)
-            {
+            while (running) { // Create loop so program doesn't have to be reopened to try again
+                
+                // Generate a sequence of numbers
                 List<int> numbers = new List<int>();
                 for (int i = 0; i < entryNumAmount; i++)
                     numbers.Add(r.Next(1, highestEntry));
-                numbers.Sort();
+                numbers.Sort(); // Sort smallest -> highest
 
+                // Write information about the generated numbers
                 Console.Write("Generated numbers");
                 for (int i = 0; i < numbers.Count; i++)
                     Console.Write(" " + numbers[i]);
@@ -28,17 +30,22 @@ namespace Lottery
                 DateTime startTime = DateTime.Now;
                 int tries = 0;
                 bool foundReplica = false;
-                while (!foundReplica)
-                {
+                while (!foundReplica) {
                     tries++;
+                    
+                    // Generate a new random sequence of numbers
                     List<int> numbers2 = new List<int>();
                     for (int i = 0; i < entryNumAmount; i++)
                         numbers2.Add(r.Next(1, highestEntry));
-                    numbers2.Sort();
+                    numbers2.Sort(); // Sort smallest -> highest
+                    
+                    // Since we sorted them we can loop through each digit and compare the numbers on the same position
                     bool found = true;
                     for (int i = 0; i < entryNumAmount; i++)
                         if (numbers[i] != numbers2[i])
                             found = false;
+                    
+                    // If it was indeed found, exit loop
                     foundReplica = found;
                 }
 
